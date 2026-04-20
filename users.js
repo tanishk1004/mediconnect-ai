9a2693039fb9b55b1b81610c7ca252b8b2a1a906
tanishk1004 <tanishkkumarsg819@gmail.com>
2026-04-20 16:13:17 +0530

    Add user profile routes with authentication

---
+++ users.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { getProfile, updateProfile } = require('../controllers/userController');
+const { protect } = require('../middleware/auth');
+router.get('/profile', protect, getProfile);
+router.put('/profile', protect, updateProfile);
+module.exports = router;
